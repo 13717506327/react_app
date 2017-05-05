cf01a6d4b86c63adf1fe5be6485d4a1f1c5fe6f1
13717506327 <13717506327@163.com>
2017-05-05 18:02:45 +0800

    react项目

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+import {Icon} from "antd-mobile"
+ReactDOM.render(
+
+<Icon type="check" size="lg" />
+  ,document.getElementById('root')
+);
